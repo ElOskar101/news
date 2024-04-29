020320f39c5074e71f263918fab5a41130b5a491
ElOskar101 <oscaresga22@gmail.com>
2024-04-28 21:42:21 -0600

    Adding Yaml files

---
+++ data_treatment.py
@@ -0,0 +1,7 @@
+class DataTreatment:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def data_treatment(self):
+        return True
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from selenium import webdriver
+from RPA.Browser.Selenium import Selenium
 import time
-
-from scrapping import scrapping
 
 
 def setup_driver():
-    options = webdriver.FirefoxOptions()
-    return webdriver.Firefox(options=options)
-
+    return Selenium()
 
 def main():
     driver = setup_driver()
     url = "https://apnews.com/"
     scrapping(driver, url)
     time.sleep(200)
-    driver.quit()
+    # driver.quit()
+
+
 
 
 # TODO:
 # Accept cookies
 # Validate elements were not found
 main()
+
+"""
+
+
+work_items = WorkItems()
+work_items.get_input_work_item()
+work_item = work_items.get_work_item_variables()
+
+{
+    "key":"value"
+}
+"""
